a34a9287dc2d0644befbddadd122d568379c8645
vedan666 <vedant.shukla123@gmail.com>
2024-11-23 00:06:00 +0530

    updated code

--- modules/azurerm_storage_account/main.tf
+++ modules/azurerm_storage_account/main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_storage_account" "stg" {
-    for_each = var.stg
-    name = each.value.stg_name
-    location = each.value.location
-    resource_group_name = each.value.resource_group_name
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+  for_each                 = var.stg
+  name                     = each.value.stg_name
+  location                 = each.value.location
+  resource_group_name      = each.value.resource_group_name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 resource "azurerm_storage_container" "blob" {
   for_each              = var.blob
   name                  = each.value.blob_name
-  storage_account_name  = each.value.stg_name
+  storage_account_id    = azurerm_storage_account.stg[each.key].id
   container_access_type = "private"
 }
