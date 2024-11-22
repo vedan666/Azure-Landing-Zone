f447f5513c8e2a7c70970ed96ece0821101b725a
vedan666 <vedant.shukla123@gmail.com>
2024-11-22 23:33:47 +0530

    Updated

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,34 +1,34 @@
 module "resource" {
-  source = "../../Modules/ResourceGroup"
+  source = "../../Modules/azurerm_resource_group"
   rg_map = var.rg_map
 }
 
 module "vnet" {
-  source = "../../Modules/Vnet"
+  source = "../../Modules/azurerm_virtual_network"
   vnet_map = var.vnet_map
   depends_on = [ module.resource ]
 }
 
 module "subnet" {
-  source = "../../Modules/Subnet"
+  source = "../../Modules/azurerm_subnet"
   subnet_map = var.subnet_map
   depends_on = [ module.vnet ]
 }
 
 module "vm" {
-  source = "../../Modules/Vm"
+  source = "../../Modules/azurerm_linux_virtual_machine"
   vm_map = var.vm_map
   depends_on = [ module.subnet , module.resource ]
 }
 
 module "pip" {
-  source = "../../Modules/Public_ip"
+  source = "../../Modules/azurerm_public_ip"
   pip_map = var.pip_map
   depends_on = [ module.vm ]
 }
 
 module "bastion" {
-  source = "../../Modules/Bastion"
+  source = "../../Modules/azurerm_bastion_host"
   bastion_map = var.bastion_map
   depends_on = [ module.vm , module.pip]
 }
