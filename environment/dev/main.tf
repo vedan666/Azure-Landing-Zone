d73f94ef0471650479f1f4de08f1213b0f468822
vedan666 <vedant.shukla123@gmail.com>
2024-11-22 23:54:37 +0530

    updated code

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,40 +1,40 @@
 module "resource" {
-  source = "../../Modules/azurerm_resource_group"
+  source = "../../modules/azurerm_resource_group"
   rg_map = var.rg_map
 }
 module "stg" {
   depends_on = [module.resource]
-  source     = "../../Modules/azurerm_storage_account"
+  source     = "../../modules/azurerm_storage_account"
   stg        = var.shared
   blob       = var.shared
 }
 
 module "vnet" {
-  source     = "../../Modules/azurerm_virtual_network"
+  source     = "../../modules/azurerm_virtual_network"
   vnet_map   = var.vnet_map
   depends_on = [module.resource]
 }
 
 module "subnet" {
-  source     = "../../Modules/azurerm_subnet"
+  source     = "../../modules/azurerm_subnet"
   subnet_map = var.subnet_map
   depends_on = [module.vnet]
 }
 
 module "vm" {
-  source     = "../../Modules/azurerm_linux_virtual_machine"
+  source     = "../../modules/azurerm_linux_virtual_machine"
   vm_map     = var.vm_map
   depends_on = [module.subnet, module.resource]
 }
 
 module "pip" {
-  source     = "../../Modules/azurerm_public_ip"
+  source     = "../../modules/azurerm_public_ip"
   pip_map    = var.pip_map
   depends_on = [module.vm]
 }
 
 module "bastion" {
-  source      = "../../Modules/azurerm_bastion_host"
+  source      = "../../modules/azurerm_bastion_host"
   bastion_map = var.bastion_map
   depends_on  = [module.vm, module.pip]
 }
